d047f807eb3f3419f6799402495bb79055784fd5
PvAb2 <estudanteifrocacoal@gmail.com>
2024-12-19 23:42:35 -0300

    Adicionando pagina de edtUsuario.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once '../PHP/validaUser.php';
-
-spl_autoload_register(function ($class) {
-    require_once "../classes/{$class}.class.php" ;
-  });
-  $usuario = new Usuario;
-
-  $usuario->logout();
